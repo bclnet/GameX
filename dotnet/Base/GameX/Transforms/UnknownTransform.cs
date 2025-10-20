a769f0a50ba1de74df21b0e8eeacd910bca27612
Sky Morey <apphaus@outlook.com>
2025-10-20 07:03:49 -0500

    updates

---
+++ dotnet/Base/GameX/Transforms/UnknownTransform.cs
@@ -0,0 +1,13 @@
+﻿using GameX.Formats.Unknown;
+using System;
+using System.Threading.Tasks;
+
+namespace GameX.Transforms;
+
+/// <summary>
+/// UnknownTransform
+/// </summary>
+public static class UnknownTransform {
+    public static bool CanTransformFileObject(PakFile left, PakFile right, object source) => false;
+    public static Task<IUnknownFileModel> TransformFileObjectAsync(PakFile left, PakFile right, object source) => throw new NotImplementedException();
+}
